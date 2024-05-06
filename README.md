2d0e51e7a46010eaa993ee6ae60005c465641ed8
Keno Krieger <kriegerk@uni-bremen.de>
2024-05-06 13:15:02 +0200

    Initialise version control

---
+++ README.md
@@ -0,0 +1,11 @@
+# wind power prediction (wpp)
+
+wpp is a package designed for predicting power output from wind turbines
+(or wind farms) using different methods ranging from physical power 
+calculations to machine learning.
+
+## Install
+
+## Usage
+
+## Development
---
+++ src/wpp/__init__.py
@@ -0,0 +1,18 @@
+"""
+wpp is a package designed for predicting power output from wind turbines
+(or wind farms) using different methods ranging from physical power calculations
+to machine learning.
+"""
+# Copyright (C) 2024  Keno Krieger <kriegerk@uni-bremen.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
