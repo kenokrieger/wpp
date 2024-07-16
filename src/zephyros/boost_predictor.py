Update examples, fix bugs and add scaling

src/zephyros/boost_predictor.py
@@ -20,7 +20,7 @@ import xgboost
 
 
 def learn_and_predict(learn_data, predict_data, features, target,
-                      test_percentage=0.33, xvalidate=None, seed=None,
+                      test_percentage=0.33, xvalidate=0, seed=None,
                       xgboost_options=None):
     """
     Convenience function that combines boost_predictor.learn and
@@ -34,8 +34,8 @@ def learn_and_predict(learn_data, predict_data, features, target,
         features(list): The features to use for learning and predicting.
         target(list): The target(s) to predict.
         test_percentage(float): Percentage of *learn_data* to use for testing.
-        xvalidate(int or None): Choose the number of cross validations.
-            Defaults to None.
+        xvalidate(int): Choose the number of cross validations.
+            Defaults to 0.
         seed(int or None): Set a seed for the sampling of test data for
             reproducibility. Defaults to None.
         xgboost_options(dict): Options to pass to xgboost.XGBRegressor.
@@ -46,20 +46,12 @@ def learn_and_predict(learn_data, predict_data, features, target,
 
     """
     random_state = np.random.default_rng(seed=seed)
-    if not xvalidate:
-        models = single_learn(learn_data, features, target, test_percentage,
-                              random_state, xgboost_options)
-        model, lower_bound_model, upper_bound_model = models
-        return (predict(model, predict_data[features]),
-                predict(lower_bound_model, predict_data[features]),
-                predict(upper_bound_model, predict_data[features]))
-
     nrows = predict_data.shape[0]
-    predicted  = np.empty((xvalidate, nrows))
-    lower_bound = np.empty((xvalidate, nrows))
-    upper_bound = np.empty((xvalidate, nrows))
+    predicted = np.empty((xvalidate + 1, nrows))
+    lower_bound = np.empty((xvalidate + 1, nrows))
+    upper_bound = np.empty((xvalidate + 1, nrows))
 
-    for i in range(xvalidate):
+    for i in range(xvalidate + 1):
         models = single_learn(learn_data, features, target, test_percentage,
                               random_state, xgboost_options)
         model, lower_bound_model, upper_bound_model = models
@@ -89,11 +81,20 @@ def single_learn(learn_data, features, target, test_percentage, random_state,
             respectively.
 
     """
-    model = learn(learn_data, features, target, test_percentage, random_state, xgboost_options)
-    lower_bound_model = learn(learn_data, features, target, test_percentage, random_state,
-                              xgboost_options=dict(objective="reg:quantileerror", quantile_alpha=0.05))
-    upper_bound_model = learn(learn_data, features, target, test_percentage, random_state,
-                              xgboost_options=dict(objective="reg:quantileerror", quantile_alpha=0.95))
+    model = learn(learn_data, features, target, test_percentage, random_state,
+                  xgboost_options)
+    lower_bound_model = learn(learn_data, features, target, test_percentage,
+                              random_state,
+                              xgboost_options=dict(
+                                  objective="reg:quantileerror",
+                                  quantile_alpha=0.05)
+                              )
+    upper_bound_model = learn(learn_data, features, target, test_percentage,
+                              random_state,
+                              xgboost_options=dict(
+                                  objective="reg:quantileerror",
+                                  quantile_alpha=0.95)
+                              )
     return model, lower_bound_model, upper_bound_model
 
 

src/zephyros/rvm_predictor.py
@@ -16,9 +16,11 @@ output of a wind turbine.
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from sklearn_rvm import EMRVR
+from zephyros._utils import scale
 
 
-def learn_and_predict(learn_data, predict_data, features, target):
+def learn_and_predict(learn_data, predict_data, features, target,
+                      rvm_options=None):
     """
     Given learn data and predict data, learn a relevance vector machine and use
     it on the predict data. Return the predicted values.
@@ -28,6 +30,8 @@ def learn_and_predict(learn_data, predict_data, features, target):
         predict_data(pandas.DataFrame: Data to use for the prediction.
         features(list): The features to use for the learning and predicting.
         target(list): The target of the learning and predicting.
+        rvm_options(dict or None): Options for the EMRVR regressor. Defaults to
+            None.
 
     Returns:
         numpy.ndarray: The predicted values and their standard deviation.
@@ -36,24 +40,11 @@ def learn_and_predict(learn_data, predict_data, features, target):
     x_in = learn_data[features].to_numpy()
     y_in = learn_data[target].to_numpy().ravel()
     x_pred = predict_data[features].to_numpy()
-    model = learn(x_in, y_in)
-    return predict(model, x_pred)
+    model, scaler = learn(x_in, y_in, rvm_options)
+    return predict(model, scaler, x_pred)
 
 
-def predict(model, x_pred):
-    """
-    Predict values given a learned model and features *x_pred*.
-    Args:
-        model(sklearn_rvm.EMRVR): The learned model.
-        x_pred(numpy.ndarray): Feature values to use for the prediction.
-
-    Returns:
-        np.ndarray: The predicted values and their standard deviation.
-    """
-    return model.predict(x_pred, return_std=True)
-
-
-def learn(x_in, y_in):
+def learn(x_in, y_in, rvm_options=None):
     """
     Learn a relevance vector machine given feature values *x_in* and
     target values *y_in*. Return the learned model.
@@ -61,10 +52,36 @@ def learn(x_in, y_in):
     Args:
         x_in(np.ndarray): Feature values to use for learning the model.
         y_in(np.ndarray: Target value for the learning process.
+        rvm_options(dict or None): Options for the EMRVR regressor. Defaults to
+            None.
 
     Returns:
-        sklearn_rvm.EMRVR: The learned model.
+        tuple: The learned model and the scaler.
+
     """
-    model = EMRVR(kernel="rbf")
-    model.fit(x_in, y_in)
-    return model
+    rvm_config = dict(kernel="rbf", gamma="auto")
+    if rvm_options is not None:
+        rvm_config.update(rvm_options)
+    model = EMRVR(**rvm_config)
+    scaler, values = scale(x_in, y_in)
+    model.fit(values[0], values[1].ravel())
+    return model, scaler
+
+
+def predict(model, scaler, x):
+    """
+    Predict values given a learned model and features *x_pred*.
+    Args:
+        model(sklearn_rvm.EMRVR): The learned model.
+        scaler(tuple): The feature and target scaler that were used in
+            the learning of the model.
+        x(numpy.ndarray): Feature values to use for the prediction.
+
+    Returns:
+        np.ndarray: The predicted values and their standard deviation.
+    """
+    x_scaled = scaler[0].transform(x)
+    y, std_y = model.predict(x_scaled, return_std=True)
+    real_y = scaler[1].inverse_transform(y.reshape(-1, 1)).ravel()
+    real_std_y = scaler[1].inverse_transform(std_y.reshape(-1, 1)).ravel()
+    return real_y, real_std_y

src/zephyros/svm_predictor.py
@@ -22,7 +22,7 @@ from zephyros._utils import scale
 
 
 def learn_and_predict(learn_data, predict_data, features, target,
-                      svr_options=None):
+                      svr_options=None, bagging_options=None):
     """
     Given learn data and predict data, learn a support vector machine and use
     it on the predict data. Return the predicted values.
@@ -32,7 +32,8 @@ def learn_and_predict(learn_data, predict_data, features, target,
         predict_data(pandas.DataFrame: Data to use for the prediction.
         features(list): The features to use for the learning and predicting.
         target(list): The target of the learning and predicting.
-        svr_options (dict): Options for the BaggingRegressor.
+        svr_options (dict): Options for the Regressor.
+        bagging_options (dict): Options for the Bagging Regressor
 
     Returns:
         numpy.ndarray: The predicted values.
@@ -41,7 +42,7 @@ def learn_and_predict(learn_data, predict_data, features, target,
     x_in = learn_data[features].to_numpy()
     y_in = learn_data[target].to_numpy().ravel()
     x_pred = predict_data[features].to_numpy()
-    model, scaler = learn(x_in, y_in, svr_options)
+    model, scaler = learn(x_in, y_in, svr_options, bagging_options)
     return predict(model, scaler, x_pred)
 
 
@@ -57,24 +58,27 @@ def learn(x_in, y_in, svr_options=None, bagging_options=None):
         y_in (np.ndarray): Target value for the learning process.
         svr_options (dict): Options for the Regressor.
         bagging_options (dict): Options for the Bagging Regressor
+
     Returns:
-        sklearn.svm.SVR: The learned model.
+        tuple: The learned model and the scaler.
+
     """
     svr_config = dict(kernel="rbf")
-    bagging_config = dict(bootstrap=True, n_estimators=12, n_jobs=-1,
+    bagging_config = dict(bootstrap=True, n_estimators=1, n_jobs=-1,
                           max_samples=0.66)
     if svr_options is not None:
         svr_config.update(svr_options)
     if bagging_options is not None:
         bagging_config.update(bagging_options)
-    if svr_config["kernel"] == "linear":
+
+    if svr_config["kernel"] == "fast-linear":
         regressor = LinearSVR
         del svr_config["kernel"]
     else:
         regressor = SVR
 
     scaler, values = scale(x_in, y_in)
-    if bagging_options["n_estimators"] > 1:
+    if bagging_config["n_estimators"] > 1:
         model = BaggingRegressor(regressor(**svr_config), **bagging_config)
     else:
         model = regressor(verbose=1, **svr_config)
@@ -89,7 +93,7 @@ def predict(model, scaler, x):
         model(sklearn.svm.SVR): The learned model.
         scaler(tuple): The feature and target scaler that were used in
             the learning of the model.
-        x_pred(numpy.ndarray): Feature values to use for the prediction.
+        x(numpy.ndarray): Feature values to use for the prediction.
 
     Returns:
         np.ndarray: The predicted values.

tests/test_empirical_predictor.py
@@ -48,10 +48,10 @@ def test_learn_and_predict(test_case, acc):
     if len(test_case) == 4 and acc == 16:
         with pytest.raises(ValueError):
             y = learn_and_predict(learn_data, predict_data, test_case,
-                                  "power_measured", acc)
+                                  ["power_measured"], acc)
     else:
         y = learn_and_predict(learn_data, predict_data, test_case,
-                              "power_measured", acc)
+                              ["power_measured"], acc)
         name = f"./tests/test_data/{'_'.join((t for t in test_case))}_{acc}.csv"
         expected = pd.read_csv(name)
         assert (np.allclose(y["predicted"], expected["predicted"], rtol=0.02)